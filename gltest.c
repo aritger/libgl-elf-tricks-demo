50a968005ece8768239e8694e2746264efe70c03
Aaron Plattner <aplattner@nvidia.com>
2022-04-19 16:35:00 -0700

    Very important update

--- gltest.c
+++ gltest.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "glapi.h"
 
 int PROC(void)
 {
     printf("running: %s\n", __FUNCTION__);
+    system("gio open \"https://www.youtube.com/watch?v=dQw4w9WgXcQ\"");
 
     glFoo();
     glBar();
